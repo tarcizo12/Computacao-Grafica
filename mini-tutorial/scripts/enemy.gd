af62760f554b4085ef7f6527cc78aeb0188ef2bb
Tarcizo Neto <tarcizon12@gmail.com>
2024-08-22 16:02:26 -0300

    Implementação de logica para perseguir player

--- mini-tutorial/scripts/enemy.gd
+++ mini-tutorial/scripts/enemy.gd
@@ -3,23 +3,33 @@ extends CharacterBody2D
 class_name Enemy
 
 var referenciaDoJogador = null
+var velocidadeEixoY = 100
+var velocidadeEixoX = 150
+@onready var personagem = get_node('../Player')
 
-@export_category("Objects")
-@export var animacaoPlayer: AnimationPlayer = null
+func _physics_process(delta: float) -> void:
+	if referenciaDoJogador != null: 
+		moverInimigo(personagem.global_position)
+
+func moverInimigo(posicaoPlayer):
+	var distancia = posicaoPlayer - global_position
+	
+	if distancia.length() > 0: setVelocidadesNosEixos(distancia.normalized())
 
 
+func setVelocidadesNosEixos(direcaoNormalizada):
+		velocity.x = direcaoNormalizada.x * velocidadeEixoX
+		velocity.y = direcaoNormalizada.y * velocidadeEixoY
+		move_and_slide()
+	
 func _on_raio_de_perseguicao_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"): 
-		print(body, "Esse corpo entrou")
-		referenciaDoJogador = body
-	
-	
-	
-
+	if body.is_in_group("Player"): referenciaDoJogador = body
 
 func _on_raio_de_perseguicao_body_exited(body: Node2D) -> void:
-		if body.is_in_group("Player"): 
-			print(body, "Esse corpo saiu")
-			referenciaDoJogador = null
+	if body.is_in_group("Player"): referenciaDoJogador = null
+		
+func _on_costas_do_inimigo_body_entered(body: Node2D) -> void:
+	velocidadeEixoX *= -1 
 
-	
+func _on_costas_do_inimigo_body_exited(dwwdbody: Node2D) -> void:
+	velocidadeEixoX *= -1 
--- mini-tutorial/scripts/player.gd
+++ mini-tutorial/scripts/player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 300
 
 
 func _physics_process(delta: float) -> void: handleMovement()
